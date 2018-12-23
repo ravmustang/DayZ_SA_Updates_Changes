81547e5903fd12c4696e84fe69fb0dc07f6243cc
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2018-12-23 08:55:46 -0600

    DayZ SA Updates 1.0.150000 mission

--- mpmissions/dayzOffline.chernarusplus/init.c
+++ mpmissions/dayzOffline.chernarusplus/init.c
@@ -4,11 +4,13 @@ void main()
 	//INIT WEATHER BEFORE ECONOMY INIT------------------------
 	Weather weather = g_Game.GetWeather();
 
-    weather.MissionWeather(false);    // false = use weather controller from Weather.c
+	weather.MissionWeather(false);    // false = use weather controller from Weather.c
 
-    weather.GetOvercast().Set( Math.RandomFloatInclusive(0.4, 0.6), 1, 0);
-    weather.GetRain().Set( 0, 0, 1);
-    weather.GetFog().Set( Math.RandomFloatInclusive(0.05, 0.1), 1, 0);
+	weather.GetOvercast().Set( Math.RandomFloatInclusive(0.4, 0.6), 1, 0);
+	weather.GetRain().Set( 0, 0, 1);
+	weather.GetFog().Set( Math.RandomFloatInclusive(0.05, 0.1), 1, 0);
+	
+	weather.GetRain().SetLimits( 0, 0.1 );
 
 	//INIT ECONOMY--------------------------------------
 	Hive ce = CreateHive();
@@ -30,6 +32,32 @@ void main()
         day = 20;
 		
 		GetGame().GetWorld().SetDate(year, month, day, hour, minute);
+	}
+	
+	//-----------------------X-mas
+	array<vector> treePositions = { "6560.29 0 2462.12",
+									"1652.66 0 14230.71",
+									"3801.06 0 8847.76",
+									"9442.32 0 8829.03",
+									"7903.16 0 12576.52",
+									"11617.75 0 14663.98",
+									"12830.08 0 10115.18",
+									"11221.94 0 12225.89",
+									"3471.93 0 12988.33",
+									"13933.42 0 13228.44",
+									"12022.64 0 9082.89",
+									"10468.54 0 2373.16",
+									"2725.48 0 5288.75",
+	};
+	
+
+	Object treeEntity;
+	for ( int i=0; i < treePositions.Count(); i++ )
+	{
+		vector treePos = treePositions[i];
+		float posY = GetGame().SurfaceY(treePos[0], treePos[2]);
+		treeEntity = GetGame().CreateObject("ChristmasTree", Vector( treePos[0], posY, treePos[2]), false);
+
 	}
 }
 
@@ -75,7 +103,7 @@ class CustomMission: MissionServer
 		{
 			itemEnt = itemTop.GetInventory().CreateInInventory("Rag");
 			if ( Class.CastTo(itemBs, itemEnt ) )
-		itemBs.SetQuantity(4);
+				itemBs.SetQuantity(4);
 
 			SetRandomHealth(itemEnt);
 			
